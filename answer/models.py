bd7955d29ebf6026b5e749dcce70ab9abdb1f765
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-03 20:38:27 +0100

    Añadido modelo de answer

---
+++ answer/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from answer.settings import ANSWERS_TYPE
+from event.models import Event
+from profile.models import Profile
+
+
+class Answers(models.Model):
+	answer = models.CharField(max_length=64)
+	type = models.CharField(max_length=2, choices=ANSWERS_TYPE)
+
+	event = models.ForeignKey(Event)
+	user = models.ForeignKey(Profile, help_text="Help to know which user created the answer")
+
+	def __unicode__(self):
+		return u'{0} ({1})'.format(self.answer, self.event.title)
