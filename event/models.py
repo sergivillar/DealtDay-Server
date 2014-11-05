6be357efdf8e45fb706e2b36afa3a8e0666b5b82
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-05 23:20:04 +0100

    Corregido dos errores en los modelos

--- event/models.py
+++ event/models.py
@@ -7,8 +7,8 @@ class Event(models.Model):
 	timestamp = models.DateTimeField(auto_now_add=True)
 	time_to_close = models.DateTimeField(help_text="When the event finish")
 	num_answers = models.IntegerField(default=1, help_text="Number of options you can select")
-	has_options = models.BooleanField()
-	has_dates = models.BooleanField()
+	has_options = models.BooleanField(default=False)
+	has_dates = models.BooleanField(default=False)
 	open = models.BooleanField(default=False, help_text="If True, users in the event can add more options")
 	voters_public = models.BooleanField(default=False, help_text="If True, others users can see what you have voted")
 
--- friends/models.py
+++ friends/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Friends(models.Model):
-	from_user = models.ForeignKey(User)
-	to_user = models.ForeignKey(User)
+	from_user = models.ForeignKey(User, related_name='friend_requests_sent')
+	to_user = models.ForeignKey(User, related_name='friend_requests_received')
 
 	def __unicode__(self):
 		return u'{0} ({1})'.format(self.from_user.username, self.to_user.username)
