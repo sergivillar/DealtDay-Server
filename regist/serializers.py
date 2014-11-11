8af950fadc444ea19e9aaf9f586081b31b44c2b7
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-11 18:49:26 +0100

    Corregida validacion registro

--- regist/serializers.py
+++ regist/serializers.py
@@ -13,6 +13,7 @@ REGEX_VALID_PASSWORD = (
 
 class RegistSerializer(serializers.ModelSerializer):
 	encrypt = serializers.CharField(read_only=True)
+	password_2 = serializers.CharField(write_only=True)
 
 	class Meta:
 		model = Regist
@@ -32,19 +33,19 @@ class RegistSerializer(serializers.ModelSerializer):
 
 	def validate(self, attrs):
 
-		if 'email' not in attrs:
-			raise serializers.ValidationError('Email is a required field')
-
 		if Regist.objects.filter(email=attrs['email']).exists():
-			raise serializers.ValidationError("This email is already registered")
+			raise serializers.ValidationError({"EmailRegistrado": "Este email ya esta registrado."})
+
+		if attrs['password'] != attrs['password_2']:
+			raise serializers.ValidationError({"PasswordDiferente": "Las passwords no coinciden."})
 
 		if len(attrs['password']) < MINIMUM_PASSWORD_LENGTH:
-			raise serializers.ValidationError('Too short password. Minimum: 6 characters')
+			raise serializers.ValidationError({"PasswordInvalida": "Password muy corta. Minimo: 6 caracteres."})
 
 		if len(attrs['password']) > MAXIMUM_PASSWORD_LENGTH:
-			raise serializers.ValidationError("Too long password. Max: 20 characters")
+			raise serializers.ValidationError({"PasswordInvalida": "Password muy larga. Maximo: 20 caracteres."})
 
 		if not self.password_valitade(attrs['password']):
-			raise serializers.ValidationError("Invalid format password (not allowed: spaces, /, \, %, @, =)")
+			raise serializers.ValidationError({"PasswordInvalida": "Formato de password invalida (no permitido: espacios, /, \, %, @, = )."})
 
 		return attrs
