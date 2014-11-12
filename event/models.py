8c70dfddd7635fb0fe2e2decb85b5ec2f4846261
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-12 20:41:48 +0100

    Corregido modelo de evento

--- event/models.py
+++ event/models.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 from django.db import models
 from profile.models import Profile
 
@@ -5,12 +7,12 @@ from profile.models import Profile
 class Event(models.Model):
 	title = models.CharField(max_length=128)
 	timestamp = models.DateTimeField(auto_now_add=True)
-	time_to_close = models.DateTimeField(help_text="When the event finish")
-	num_answers = models.IntegerField(default=1, help_text="Number of options you can select")
+	time_to_close = models.DateTimeField(help_text="Cuando finaliza el evento")
+	num_answers = models.IntegerField(default=1, help_text="Número de opciones que se pueden elegir")
 	has_options = models.BooleanField(default=False)
 	has_dates = models.BooleanField(default=False)
-	open = models.BooleanField(default=False, help_text="If True, users in the event can add more options")
-	voters_public = models.BooleanField(default=False, help_text="If True, others users can see what users have voted")
+	open = models.BooleanField(default=False, help_text="Si 'True', los usuarios invitados al evento pueden añadir mas opciones a la votación")
+	voters_public = models.BooleanField(default=False, help_text="Si 'True', otros usuarios pueden ver quieén ha votado cada opción")
 
 	owner = models.ForeignKey(Profile)
 
