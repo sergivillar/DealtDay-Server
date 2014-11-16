3be3beab96251518ebd5d6b637c49b1e1024d84c
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-16 19:35:16 +0100

    Modificado el admin

--- friends/admin.py
+++ friends/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from friends.models import Friend
+from friends.models import Friend, FriendRequest
 
 admin.site.register(Friend)
+admin.site.register(FriendRequest)
