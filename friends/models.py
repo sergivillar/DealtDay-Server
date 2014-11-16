bde6b3ec4441105bd0f8a4fae949758a48af78c5
Sergio Fernández <sergio.fernandezj@gmail.com>
2014-11-16 19:35:27 +0100

    Añadido nuevo modelo

--- friends/models.py
+++ friends/models.py
@@ -1,10 +1,38 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.db import transaction
+from profile.models import Profile
 
 
-class Friend(models.Model):
-	from_user = models.ForeignKey(User, related_name='friend_requests_sent')
-	to_user = models.ForeignKey(User, related_name='friend_requests_received')
+class FriendRequestManager(models.Manager):
+	def accept_request(self, friend_request):
+		""" Pasar objeto FriendRequest a True y crear Friend del usuario activo """
+		with transaction.atomic():
+			friend_request.accepted = True
+			friend_request.save()
+			Friend.objects.create(from_friend=friend_request.from_friend, to_friend=friend_request.to_friend)
+
+class FriendRequest(models.Model):
+	from_friend = models.ForeignKey(Profile, related_name='friendship_requests_sent')
+	to_friend = models.ForeignKey(Profile, related_name='friendship_requests_received')
+	timestamp = models.DateTimeField(auto_now_add=True)
+	accepted = models.BooleanField(default=False)
+
+	objects = FriendRequestManager()
+
+	class Meta:
+		unique_together = (('to_friend', 'from_friend'), )
 
 	def __unicode__(self):
 		return u'{0} ({1})'.format(self.from_user.username, self.to_user.username)
+
+
+class Friend(models.Model):
+	from_friend = models.ForeignKey(Profile, related_name='from_friend_set')
+	to_friend = models.ForeignKey(Profile, related_name='to_friend_set')
+	created = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		unique_together = (('to_friend', 'from_friend'), )
+
+	def __unicode__(self):
+		return u'{0} ({1})'.format(self.from_user.email, self.to_user.email)
