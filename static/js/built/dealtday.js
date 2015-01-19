dc247fb45cc7fd468b62d998ad3a0832bfbb6e1f
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-01-19 23:32:18 +0100

    Añadida navegación, templates y logout

--- static/js/built/dealtday.js
+++ static/js/built/dealtday.js
@@ -592,7 +592,8 @@ function(){"use strict";function e(e){function t(t,n,r,o,a){function i(){n.attr(
 (function () {
 
     var app = angular.module('dealtday', [
-        'ngMaterial'
+        'ngMaterial',
+        'ngRoute'
     ]);
 
     app.config(['$httpProvider', function ($httpProvider) {
@@ -600,19 +601,79 @@ function(){"use strict";function e(e){function t(t,n,r,o,a){function i(){n.attr(
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
     }]);
 
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '/static/templates/landing.html'
+        }).when('/eventos/', {
+            templateUrl: '/static/event/templates/event.html'
+        }).when('/amigos/', {
+            templateUrl: '/static/friends/templates/friends.html'
+        }).when('/perfil/', {
+            templateUrl: '/static/profile/templates/profile.html'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }]);
+
     app.config(function ($mdThemingProvider) {
+        $mdThemingProvider.definePalette('dealtdayColors', {
+            '50': 'DEE7E9',
+            '100': 'B0C6CB',
+            '200': '81A4AC',
+            '300': '4D7F8A',
+            '400': '276471',
+            '500': '024959',
+            '600': '014251',
+            '700': '013B48',
+            '800': '01333F',
+            '900': '01262E',
+            'A100': '05C6F2',
+            'A200': '0488A5',
+            'A400': '025366',
+            'A700': '01343F',
+            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                                // on this palette should be dark or light
+            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                '200', '300', '400', 'A100'],
+            'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        });
         $mdThemingProvider.theme('default')
-            .primaryColor('red', {
-                'default': '400', // by default use shade 400 from the pink palette for primary intentions
-                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
-            })
-            // If you specify less than all of the keys, it will inherit from the
-            // default shades
-            .accentColor('green', {
-            });
+            .primaryColor('dealtdayColors')
+            .accentColor('red')
+            .warnColor('red');
     });
+
+    app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleLeft = function () {
+            $mdSidenav('left').toggle();
+        };
+        $scope.toggleRight = function () {
+            $mdSidenav('right').toggle()
+                .then(function () {
+                    $log.debug("toggle RIGHT is done");
+                });
+        };
+
+    })
+        .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log, $location) {
+
+            $scope.navLinks = [
+                {url: 'eventos', Title: 'Eventos'},
+                {url: 'amigos', Title: 'Amigos'},
+                {url: 'perfil', Title: 'Perfil'}
+            ];
+
+            $scope.close = function () {
+                $mdSidenav('left').close();
+            };
+
+            $scope.navClass = function (page) {
+                console.log($location.path().substring(1));
+                var currentRoute = $location.path().substring(1).split('/')[0] || 'eventos';
+                return page === currentRoute ? 'active' : '';
+            };
+
+        });
 
 })();
 (function () {
