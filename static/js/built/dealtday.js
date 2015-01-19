7c3eb8413ce434d8300d5454c7074efde77f3b52
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-01-19 23:47:43 +0100

    Added directive to get key events

--- static/js/built/dealtday.js
+++ static/js/built/dealtday.js
@@ -968,3 +968,16 @@ angular.module('profile').directive('compareTo', function () {
         }
     }
 });
+angular.module('profile').directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
