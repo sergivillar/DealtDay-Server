800615092f35d36ccb505e7d80635d50849de0bc
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-01-26 21:54:44 +0100

    Can vote simple

--- static/js/built/dealtday.js
+++ static/js/built/dealtday.js
@@ -1559,6 +1559,74 @@ angular.module('event')
         $scope.initVotes();
 
     }]);
+angular.module('event')
+    .controller('EventDetailSimpleCtrl', ['$scope', 'Event', '$location', '$http', 'voteApi', '$mdDialog', function ($scope, Event, $location, $http, voteApi, $mdDialog) {
+        $scope.loading_simple = false;
+        $scope.voteId = [];
+
+        $scope.goEvent = function () {
+            $location.path('/eventos/');
+        };
+
+        $scope.vote = function () {
+            var dict = {};
+            if ($scope.voteText != null)
+                $scope.voteId.push($scope.voteText);
+            if ($scope.voteDate)
+                $scope.voteId.push($scope.voteDate);
+            dict['votes'] = $scope.voteId;
+
+            if ($scope.voteId.length == 0) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('ERROR')
+                        .content('Selecciona primero una opción para votar.')
+                        .ariaLabel('Selecciona opciones votar')
+                        .ok('OK')
+                        .targetEvent()
+                );
+            } else {
+                $scope.loading_simple = true;
+
+                $http.post(voteApi, dict)
+                    .success(function () {
+                        Event.detail({id: $scope.id}, function (data) {
+                            $scope.event = data;
+                            $scope.getMyVotes();
+                            $scope.loading_simple = false;
+                        }, function (error) {
+                            console.log(error);
+                            $scope.loading_simple = false;
+                        });
+                    })
+                    .error(function (error) {
+                        console.log(error);
+                        $scope.loading_simple = false;
+                    });
+            }
+        };
+
+        $scope.clickText = function (answer) {
+            $scope.voteText = answer.id;
+        };
+
+        $scope.clickDate = function (answer) {
+            $scope.voteDate = answer.id;
+        };
+
+        $scope.initVotes = function () {
+            angular.forEach($scope.myVotes, function (value) {
+                console.log(value);
+                if (value.type == 'TX') {
+                    $scope.voteText = value.vote;
+                } else if (value.type == 'DT') {
+                    $scope.voteDate = value.vote;
+                }
+            });
+        };
+
+        $scope.initVotes();
+    }]);
 var app = angular.module('answer', ['mdDateTime', 'ngMessages']);
 
 app.value('ANSWER_TYPES', [
