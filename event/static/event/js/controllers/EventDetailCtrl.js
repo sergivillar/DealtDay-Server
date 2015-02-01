df059d3a6e59e89718406e9787a2a84c74df5579
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-02-01 13:35:55 +0100

    Fix templates

--- event/static/event/js/controllers/EventDetailCtrl.js
+++ event/static/event/js/controllers/EventDetailCtrl.js
@@ -1,153 +1,154 @@
 angular.module('event')
     .controller('EventDetailCtrl', ['$scope', 'Event', '$location', '$routeParams', '$http', 'getMyVotes', '$mdDialog', 'Answer', 'ANSWER_TYPES', '$q', 'getFriends',
         function ($scope, Event, $location, $routeParams, $http, getMyVotes, $mdDialog, Answer, ANSWER_TYPES, $q, getFriends) {
-        var self = this;
+            var self = this;
 
-        $scope.loading = true;
-        $scope.inviteFriend = false;
-        $scope.createMode = false;
-        $scope.inviteMode = false;
-        $scope.id = $routeParams.id;
-        $scope.type_text = ANSWER_TYPES[0].name;
+            $scope.data = [];
+            $scope.loading = true;
+            $scope.inviteFriend = false;
+            $scope.createMode = false;
+            $scope.inviteMode = false;
+            $scope.id = $routeParams.id;
+            $scope.type_text = ANSWER_TYPES[0].name;
 
-        $scope.answer = new Answer();
-        $scope.answer.type = ANSWER_TYPES[0].type;
+            $scope.answer = new Answer();
+            $scope.answer.type = ANSWER_TYPES[0].type;
 
-        this.successGetDetail = function (data) {
-            $scope.event = data;
-            $scope.answer.event = $scope.event.id;
-            if ($scope.event.voters_public) {
-                angular.forEach($scope.event.answer, function (value) {
-                    value.votes = value.votes.length;
-                });
-            }
-        };
+            this.successGetDetail = function (data) {
+                $scope.event = data;
+                $scope.answer.event = $scope.event.id;
+                if ($scope.event.voters_public) {
+                    angular.forEach($scope.event.answer, function (value) {
+                        value.votes = value.votes.length;
+                    });
+                }
+            };
 
-        $scope.next = function () {
-            $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2);
-        };
-        $scope.previous = function () {
-            $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
-        };
+            $scope.next = function () {
+                $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2);
+            };
+            $scope.previous = function () {
+                $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+            };
 
-        $scope.getEventDetail = function () {
-            return Event.detail({id: $scope.id}).$promise;
-        };
+            $scope.getEventDetail = function () {
+                return Event.detail({id: $scope.id}).$promise;
+            };
 
-        $scope.getMyVotes = function () {
-            $http.get(getMyVotes + $scope.id)
-                .success(function (data) {
-                    $scope.myVotes = data;
-                })
-                .error(function (error) {
-                    console.log(error);
-                    $scope.loading = false;
-                });
-        };
-
-        $scope.getFriends = function () {
-            $http.get(getFriends)
-                .success(function (data) {
-                    $scope.friends = data;
-                    $scope.loading = false;
-                })
-                .error(function (error) {
-                    console.log(error);
-                    $scope.loading = false;
-                });
-        };
-
-        $scope.displayType = function (type) {
-            if (!type) {
-                $scope.answer.type = ANSWER_TYPES[0].type;
-                var message = ANSWER_TYPES[0].name;
-            } else {
-                $scope.answer.type = ANSWER_TYPES[1].type;
-                var message = ANSWER_TYPES[1].name;
-            }
-            $scope.answer.answer = '';
-            $scope.type_text = message;
-        };
-
-        $scope.addDate = function (ev) {
-            $mdDialog.show({
-                controller: DateTimePicker,
-                templateUrl: '/static/event/templates/dialog-time-picker.html',
-                targetEvent: ev
-            })
-                .then(function (data) {
-                    $scope.answer.time = data.time;
-                    $scope.answer.answer = data.timeFormatted;
-                }, function () {
-                });
-        };
-
-        $scope.add = function () {
-            $scope.$broadcast('partial_loading_start');
-
-            $scope.answer.$save(function (data) {
-                var type = $scope.answer.type;
-                $scope.answer = new Answer();
-                $scope.answer.type = type;
-
-                $scope.getEventDetail()
-                    .then(function (data) {
-                        self.successGetDetail(data);
-                    }, function (error) {
+            $scope.getMyVotes = function () {
+                $http.get(getMyVotes + $scope.id)
+                    .success(function (data) {
+                        $scope.myVotes = data;
+                    })
+                    .error(function (error) {
                         console.log(error);
+                        $scope.loading = false;
+                    });
+            };
+
+            $scope.getFriends = function () {
+                $http.get(getFriends)
+                    .success(function (data) {
+                        $scope.friends = data;
+                        $scope.loading = false;
                     })
-                    .then(function () {
-                        $scope.getMyVotes();
-                    })
-                    .then($scope.$broadcast('partial_loading_end'));
+                    .error(function (error) {
+                        console.log(error);
+                        $scope.loading = false;
+                    });
+            };
 
-            }, function (error) {
-                console.log(error);
+            $scope.displayType = function (type) {
+                if (!type) {
+                    $scope.answer.type = ANSWER_TYPES[0].type;
+                    var message = ANSWER_TYPES[0].name;
+                } else {
+                    $scope.answer.type = ANSWER_TYPES[1].type;
+                    var message = ANSWER_TYPES[1].name;
+                }
+                $scope.answer.answer = '';
+                $scope.type_text = message;
+            };
 
-                $scope.$broadcast('partial_loading_end');
-            });
-        };
+            $scope.addDate = function (ev) {
+                $mdDialog.show({
+                    controller: DateTimePicker,
+                    templateUrl: '/static/event/templates/dialog-time-picker.html',
+                    targetEvent: ev
+                })
+                    .then(function (data) {
+                        $scope.answer.time = data.time;
+                        $scope.answer.answer = data.timeFormatted;
+                    }, function () {
+                    });
+            };
 
-        $scope.sendInvitation = function () {
-            console.log("Enviar invitacion");
-        };
+            $scope.add = function () {
+                $scope.$broadcast('partial_loading_start');
 
-        $scope.selectText = function (answer) {
-            $scope.voteText = answer.id;
-        };
+                $scope.answer.$save(function (data) {
+                    var type = $scope.answer.type;
+                    $scope.answer = new Answer();
+                    $scope.answer.type = type;
 
-        $scope.selectDate = function (answer) {
-            $scope.voteDate = answer.id;
-        };
+                    $scope.getEventDetail()
+                        .then(function (data) {
+                            self.successGetDetail(data);
+                        }, function (error) {
+                            console.log(error);
+                        })
+                        .then(function () {
+                            $scope.getMyVotes();
+                        })
+                        .then($scope.$broadcast('partial_loading_end'));
 
-        $scope.getEventDetail()
-            .then(function (data) {
-                self.successGetDetail(data);
-            }, function (error) {
-                console.log(error);
-                $scope.loading = false;
-            })
-            .then(function () {
-                $scope.getMyVotes();
-            })
-            .then(function () {
-                $scope.getFriends();
-            });
+                }, function (error) {
+                    console.log(error);
 
-        $scope.showCreateMode = function () {
-            $scope.createMode ? $scope.createMode = false : $scope.createMode = true;
-        };
+                    $scope.$broadcast('partial_loading_end');
+                });
+            };
 
-        $scope.showInviteMode = function () {
-            $scope.inviteMode ? $scope.inviteMode = false : $scope.inviteMode = true;
-        };
+            $scope.sendInvitation = function () {
+                console.log("Enviar invitacion");
+            };
 
-        $scope.inviteButton = function (data) {
-            $scope.inviteFriend = data;
-            data ? $scope.createMode = false : $scope.inviteMode = false;
-        };
+            $scope.selectText = function (answer) {
+                $scope.voteText = answer.id;
+            };
 
-    }]);
+            $scope.selectDate = function (answer) {
+                $scope.voteDate = answer.id;
+            };
+
+            $scope.getEventDetail()
+                .then(function (data) {
+                    self.successGetDetail(data);
+                }, function (error) {
+                    console.log(error);
+                    $scope.loading = false;
+                })
+                .then(function () {
+                    $scope.getMyVotes();
+                })
+                .then(function () {
+                    $scope.getFriends();
+                });
+
+            $scope.showCreateMode = function () {
+                $scope.createMode ? $scope.createMode = false : $scope.createMode = true;
+            };
+
+            $scope.showInviteMode = function () {
+                $scope.inviteMode ? $scope.inviteMode = false : $scope.inviteMode = true;
+            };
+
+            $scope.inviteButton = function (data) {
+                $scope.inviteFriend = data;
+                data ? $scope.createMode = false : $scope.inviteMode = false;
+            };
+
+        }]);
 
 
 function DateTimePicker($scope, $mdDialog, $filter) {
