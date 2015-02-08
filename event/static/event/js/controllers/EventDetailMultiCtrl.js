707f10d2f112923cb55c3ed6cff423349c1c28da
Sergio Fernández <sergio.fernandezj@gmail.com>
2015-02-08 14:29:23 +0100

    refactor

--- event/static/event/js/controllers/EventDetailMultiCtrl.js
+++ event/static/event/js/controllers/EventDetailMultiCtrl.js
@@ -26,79 +26,121 @@ angular.module('event')
                 }
             };
 
+            var vote = function ($scope) {
+                var sendVote = function () {
+                        var data = $scope.votesText.concat($scope.votesDate);
+                        var dict = {};
+                        dict['votes'] = data;
+
+                        return $http.post(voteApi, dict);
+                    },
+                    refreshEvent = function () {
+                        return $scope.getEventDetail()
+                            .then(function (data) {
+                                self.successGetDetail(data.data);
+                            }, function (error) {
+                                console.log(error);
+                                $scope.loading_multi = false;
+                            });
+                    },
+                    refreshVotes = function () {
+                        $scope.votesText = [];
+                        $scope.votesDate = [];
+                        return $scope.getMyVotes();
+                    };
+
+                sendVote()
+                    .then(refreshEvent)
+                    .then(refreshVotes);
+            }
+
             $scope.vote = function () {
-                var data = $scope.votesText.concat($scope.votesDate);
-                var dict = {};
-                dict['votes'] = data;
+                $scope.loading_multi = true;
+                var sendVote = function () {
+                        var data = $scope.votesText.concat($scope.votesDate);
+                        var dict = {};
+                        dict['votes'] = data;
 
-                if (data.length == 0) {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .title('ERROR')
-                            .content('Selecciona primero una opción para votar.')
-                            .ariaLabel('Selecciona opciones votar')
-                            .ok('OK')
-                            .targetEvent()
-                    );
-                } else {
-                    $scope.loading_multi = true;
+                        return $http.post(voteApi, dict);
+                    },
+                    refreshEvent = function () {
+                        return $scope.getEventDetail()
+                            .then(function (data) {
+                                self.successGetDetail(data.data);
+                            }, function (error) {
+                                console.log(error);
+                                $scope.loading_multi = false;
+                            });
+                    },
+                    refreshVotes = function () {
+                        $scope.votesText = [];
+                        $scope.votesDate = [];
+                        return $scope.getMyVotes();
+                    };
 
-                    $http.post(voteApi, dict)
-                        .success(function () {
+                sendVote()
+                    .then(refreshEvent)
+                    .then(refreshVotes)
+                    .then(function () {
+                        $scope.loading_multi = false;
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .content('Votos guardados')
+                                .position('bottom right')
+                                .hideDelay(1500)
+                        );
+                    });
+                /*var data = $scope.votesText.concat($scope.votesDate);
+                 var dict = {};
+                 dict['votes'] = data;
 
-                            $scope.getEventDetail()
-                                .then(function (data) {
-                                    self.successGetDetail(data.data);
-                                }, function (error) {
-                                    console.log(error);
-                                    $scope.loading_multi = false;
-                                })
-                                .then(function () {
-                                    $scope.votesText = [];
-                                    $scope.votesDate = [];
-                                    $scope.getMyVotes();
-                                })
-                                .then(function () {
-                                    $scope.loading_multi = false;
-                                    $mdToast.show(
-                                        $mdToast.simple()
-                                            .content('Votos guardados')
-                                            .position('bottom right')
-                                            .hideDelay(1500)
-                                    );
-                                });
-                            /*Event.detail({id: $scope.id}, function (data) {
-                             $scope.event = data;
+                 if (data.length == 0) {
+                 $mdDialog.show(
+                 $mdDialog.alert()
+                 .title('ERROR')
+                 .content('Selecciona primero una opción para votar.')
+                 .ariaLabel('Selecciona opciones votar')
+                 .ok('OK')
+                 .targetEvent()
+                 );
+                 } else {
+                 $scope.loading_multi = true;
 
-                             if ($scope.event.voters_public) {
-                             angular.forEach($scope.event.answer, function (value) {
-                             value.votes_owners = value.votes;
-                             value.votes = value.votes.length;
-                             });
-                             }
+                 $http.post(voteApi, dict)
+                 .success(function () {
 
-                             $scope.votesText = [];
-                             $scope.votesDate = [];
-
-                             $scope.getMyVotes();
-                             $scope.loading_multi = false;
-                             $mdToast.show(
-                             $mdToast.simple()
-                             .content('Votos guardados')
-                             .position('bottom right')
-                             .hideDelay(1500)
-                             );*/
-                        }, function (error) {
-                            console.log(error);
-                            $scope.loading_multi = false;
-                            $mdToast.show(
-                                $mdToast.simple()
-                                    .content('Error al votar')
-                                    .position('bottom right')
-                                    .hideDelay(1500)
-                            );
-                        });
-                }
+                 $scope.getEventDetail()
+                 .then(function (data) {
+                 self.successGetDetail(data.data);
+                 }, function (error) {
+                 console.log(error);
+                 $scope.loading_multi = false;
+                 })
+                 .then(function () {
+                 $scope.votesText = [];
+                 $scope.votesDate = [];
+                 $scope.getMyVotes();
+                 })
+                 .then(function () {
+                 $scope.loading_multi = false;
+                 $mdToast.show(
+                 $mdToast.simple()
+                 .content('Votos guardados')
+                 .position('bottom right')
+                 .hideDelay(1500)
+                 );
+                 });
+                 }, function (error) {
+                 console.log(error);
+                 $scope.loading_multi = false;
+                 $mdToast.show(
+                 $mdToast.simple()
+                 .content('Error al votar')
+                 .position('bottom right')
+                 .hideDelay(1500)
+                 );
+                 });
+                 }*/
             };
 
             $scope.initVotes = function () {
